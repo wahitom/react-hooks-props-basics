8ff019196822d9e39f4840c1b46f5a83c3d0ec9f
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-25 23:59:33 +0300

    Completed

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -9,6 +9,7 @@ function BlogPost() {
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText='This was a good lesson' />
     </div>
   );
 }
